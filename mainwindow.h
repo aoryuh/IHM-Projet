146f3f40f3f77551ed05ce2f966449cfbd77bac0
Daryun <42786244+aoryuh@users.noreply.github.com>
2022-01-30 17:08:57 +0100

    Update mainwindow.h
    debut d'arbre

--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtCore>
+#include <QtGui>
+#include <QFileSystemModel>
 
-QT_BEGIN_NAMESPACE
+
 namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +17,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_treeView_clicked(const QModelIndex &index);
+
 private:
     Ui::MainWindow *ui;
+    QFileSystemModel *dirmodel;
+    QFileSystemModel *filemodel;
 };
 #endif // MAINWINDOW_H
